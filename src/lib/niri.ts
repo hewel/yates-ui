dd630002c787c82ee2985a83fe56541148b566df
hewel <hewel@vivaldi.net>
2026-03-17 19:15:06 +0800

    feat: add immer for state management and implement reducer in NiriService

--- src/lib/niri.ts
+++ src/lib/niri.ts
@@ -1,12 +1,41 @@
-import Gio from "gi://Gio"
-import GLib from "gi://GLib"
-
 import GObject, { register, property } from "gnim/gobject"
 
-import { Effect, Stream, Console, Reducer } from "effect"
+import { Effect, Stream, SubscriptionRef } from "effect"
+import { produce } from "immer"
 
 import * as Niri from "./niri.types"
 import { eventStream, sendMessage } from "./socket"
+
+export type NiriState = {
+  focusedWindow: Niri.Window
+  windows: Niri.Window[]
+  focusedWorkspace: Niri.Workspace
+  workspaces: Niri.Workspace[]
+  layout: Niri.WindowLayout
+}
+
+const reducer = (event: Niri.Event) =>
+  produce<NiriState>((state) => {
+    switch (event.type) {
+      case "WindowsChanged":
+        state.windows = event.windows
+        break
+      case "WindowFocusChanged":
+        state.focusedWindow = state.windows.find((w) => w.id === event.id) ?? ({} as Niri.Window)
+        break
+      case "WindowLayoutsChanged":
+        break
+      // Handle other events as needed...
+    }
+  })
+
+const stateRef = SubscriptionRef.make<NiriState>({
+  focusedWindow: {} as Niri.Window,
+  windows: [],
+  focusedWorkspace: {} as Niri.Workspace,
+  workspaces: [],
+  layout: {} as Niri.WindowLayout,
+})
 
 @register()
 export class NiriService extends GObject.Object {
@@ -44,21 +73,10 @@ export class NiriService extends GObject.Object {
 
   private startWatchSocket() {
     eventStream.pipe(
-      Stream.runForEach((event) => Effect.sync(() => this.handleEvent(event))),
+      Stream.runForEach((event) =>
+        stateRef.pipe(Effect.flatMap((state) => SubscriptionRef.update(state, reducer(event)))),
+      ),
       Effect.runPromise,
     )
-  }
-  private handleEvent(event: Niri.Event) {
-    switch (event.type) {
-      case "WindowsChanged":
-        this.windows = event.windows
-        break
-      case "WindowFocusChanged":
-        this.focusedWindow = this.windows.find((w) => w.id === event.id) ?? ({} as Niri.Window)
-        break
-      case "WindowLayoutsChanged":
-        break
-      // Handle other events as needed...
-    }
   }
 }
